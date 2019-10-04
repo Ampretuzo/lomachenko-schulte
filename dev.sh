51db31398a0b5aaa226f50ae0377cca78394ac10
Sergi Chalauri <sergi.chalauri@gmail.com>
2019-10-04 23:51:35 +0400

    Install instructions in `dev.sh`

--- dev.sh
+++ dev.sh
@@ -1,1 +1,11 @@
-live-server --port=1111 --no-browser
+#!/usr/bin/env sh
+
+if ! [ -x "$(command -v live-server)" ]; then
+    Color_Off="\033[0m"
+    Blue="\033[0;34m"
+    echo 'live-server not installed!' >&2
+    echo -e "Run ${Blue}npm install -g live-server${Color_Off} to install it." >&2
+    exit 1
+fi
+
+live-server --port=1111 # --no-browser
